f8cc5a94189a7f30e47900608d7fe91ad68f6300
Rolly Maulana Awangga <awangga@gmail.com>
2023-09-10 18:47:19 +0700

    disk space

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 func main() {
 	// Getting filesystem statistics
 	disk := DiskUsage("/")
-	fmt.Printf("All: %.2f GB\n", float64(disk.All)/float64(GB))
-	fmt.Printf("Used: %.2f GB\n", float64(disk.Used)/float64(GB))
-	fmt.Printf("Free: %.2f GB\n", float64(disk.Free)/float64(GB))
+	all := fmt.Sprintf("All: %.2f GB\n", float64(disk.All)/float64(GB))
+	used := fmt.Sprintf("Used: %.2f GB\n", float64(disk.Used)/float64(GB))
+	free := fmt.Sprintf("Free: %.2f GB\n", float64(disk.Free)/float64(GB))
+
+	msg := "*CrootStat*\n"
+	msg = msg + "_Disk Space Status_\n"
+	msg = msg + all + used + free
+	fmt.Println(msg)
 }
