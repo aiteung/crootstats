aca9605d796dbf252f12da2005fa401bfa009165
Rolly Maulana Awangga <awangga@gmail.com>
2023-09-10 18:37:07 +0700

    disk usage

---
+++ crootstat.go
@@ -0,0 +1,33 @@
+package main
+
+import "syscall"
+
+// Creating structure for DiskStatus
+type DiskStatus struct {
+	All  uint64 `json:"All"`
+	Used uint64 `json:"Used"`
+	Free uint64 `json:"Free"`
+}
+
+// Function to get
+// disk usage of path/disk
+func DiskUsage(path string) (disk DiskStatus) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		return
+	}
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+	return
+}
+
+// Defining constants to convert size units
+const (
+	B  = 1
+	KB = 1024 * B
+	MB = 1024 * KB
+	GB = 1024 * MB
+	TB = 1024 * GB
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aiteung/crootstats
+
+go 1.21.1
