c46ca59c695af00b1d326b3d9f71201a9a37eec2
Rolly Maulana Awangga <awangga@gmail.com>
2023-09-10 19:14:39 +0700

    s

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	used := fmt.Sprintf("Used: %.2f GB\n", float64(disk.Used)/float64(GB))
 	free := fmt.Sprintf("Free: %.2f GB\n", float64(disk.Free)/float64(GB))
 
-	msg := "*CrootStat*\n"
+	msg := "*CrootStats*\n"
 	msg = msg + "_Disk Space Status_\n" + hostname + "\n"
 	msg = msg + all + used + free
 	fmt.Println(msg)
